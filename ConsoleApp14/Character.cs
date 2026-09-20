8469e9e906c05495b6b62530dac16c84849e3eb6
superilushay404-cyber <superilushay404@gmail.com>
2026-09-20 19:25:48 +0300

    feature: add damage

--- ConsoleApp14/Character.cs
+++ ConsoleApp14/Character.cs
@@ -4,21 +4,24 @@ namespace ConsoleApp14
 {
     internal class Character
     {
-        public Character(string name, float currentHealth,  int fullHealth, int xp, int lvl)
+        public Character(string name, float currentHealth, int fullHealth, int xp, float damage, int lvl)
         {
             Name = name;
             CurrentHealth = currentHealth;
             FullHealth = fullHealth;
             XP = xp;
+            Damage = damage;
             Lvl = lvl;
         }
         public string Name { get; set; }
 
         public float CurrentHealth { get; set; }
 
-        public int FullHealth { get; set; }
+        public float FullHealth { get; set; }
 
         public int XP { get; set; }
+
+        public float Damage { get; set; }
 
         public int Lvl { get; set; }
     }
