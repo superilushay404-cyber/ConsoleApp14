43676c519b85f658c0906b149186de4ef0a009a5
superilushay404-cyber <superilushay404@gmail.com>
2026-09-20 19:19:17 +0300

    feature: add class "character"

---
+++ ConsoleApp14/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.XPath;
+
+namespace ConsoleApp14
+{
+    internal class Character
+    {
+        public Character(string name, float currentHealth,  int fullHealth, int xp, int lvl)
+        {
+            Name = name;
+            CurrentHealth = currentHealth;
+            FullHealth = fullHealth;
+            XP = xp;
+            Lvl = lvl;
+        }
+        public string Name { get; set; }
+
+        public float CurrentHealth { get; set; }
+
+        public int FullHealth { get; set; }
+
+        public int XP { get; set; }
+
+        public int Lvl { get; set; }
+    }
+}
